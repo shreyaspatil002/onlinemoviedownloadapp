58b19c8b547a974b1e9eeabc4849837243d6ac8b
shreyaspatil002 <131358397+shreyaspatil002@users.noreply.github.com>
2023-06-27 12:47:37 +0530

    Add files via upload

---
+++ app/mailers/product_mailer.rb
@@ -0,0 +1,8 @@
+class ProductMailer < ApplicationMailer
+    def notify_product_creation(product_id)
+        @product = Product.find(product_id)
+        mail(to: "test@gmail.com", subject: "Product #{@product.name} has been created")
+        
+       
+    end
+end
