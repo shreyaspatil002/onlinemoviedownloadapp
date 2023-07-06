37c70aece0216ea54b2e1bf5964139cc332f8f05
shreyaspatil002 <131358397+shreyaspatil002@users.noreply.github.com>
2023-07-06 12:37:27 +0530

    Delete app directory

--- app/mailers/product_mailer.rb
+++
@@ -1,8 +0,0 @@
-class ProductMailer < ApplicationMailer
-    def notify_product_creation(product_id)
-        @product = Product.find(product_id)
-        mail(to: "test@gmail.com", subject: "Product #{@product.name} has been created")
-        
-       
-    end
-end
