a0cc40cf1810298462d9e4759693fa9f5e9426e0
shreyaspatil002 <131358397+shreyaspatil002@users.noreply.github.com>
2023-07-06 12:39:12 +0530

    Add files via upload

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,65 @@
+class ProductsController < ApplicationController
+    before_action :load_product, only: ['edit', 'update', 'show', 'destroy', 'download_pdf']
+    before_action :authenticate_user!, except: [:index]
+   # layout 'new_layout' #appiled for all
+    layout 'new_layout', except: [:index]
+   
+        def index
+            # @product= policy_scope(Product).all
+            @products= if params[:q] && !params[:q].blank?   
+                Product.matching(params[:q]).page(params[:page])
+            else 
+                Product.all.page(params[:page])
+            end
+          
+        end 
+        def new
+            @product = Product.new
+        end
+        def create
+            @product =Product.new(product_params)
+            authorize @product
+            if @product.save
+                ProductMailer.notify_product_creation(@product.id).deliver_now
+                redirect_to products_path, notice: 'product created !!!'
+            else
+                render 'new'
+            end
+        end
+     
+        def edit 
+            authorize @product
+        end
+        def update
+          
+            if @product.update(product_params)
+                redirect_to products_path, notice: 'product updated !!!'
+            else
+                render 'edit'
+            end 
+        end
+        def show
+            
+        end
+        def destroy
+            authorize @product
+            @product.destroy
+            redirect_to products_path, notice: 'product deleted !!!'
+        end  
+        def download_pdf
+            
+            send_file("#{Rails.root}/public/test1.txt",
+                        filename: "#{@product.name}.txt",
+                        type: "application/txt")
+        end
+        def self.disable_all
+            update_all(is_active: false)
+        end
+        private  
+        def product_params
+            params.require(:product).permit(:name, :code, :price, :is_active, :description, :cover_image)
+        end    
+        def load_product
+            @product =Product.find(params[:id])
+        end
+end   
---
+++ app/policies/review_policy.rb
@@ -0,0 +1,14 @@
+class ReviewPolicy < ApplicationPolicy
+    class Scope < Scope
+      def resolve 
+       scope.all
+      end
+    end
+      def create?
+        @user.role !="admin"
+      end
+      def destroy?
+       @user.role =="admin"
+      end
+  end
+  
