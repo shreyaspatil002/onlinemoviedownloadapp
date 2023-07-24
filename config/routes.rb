64e978986b338ef1dd5d53a6f9a45bf2091d609e
shreyaspatil002 <131358397+shreyaspatil002@users.noreply.github.com>
2023-07-24 14:06:51 +0530

    Add files via upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,24 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  #resources :categories
+  # resources :categories
   get 'cart', to: 'cart#show'
   get 'cart_pay', to: 'cart#pay'
+  get 'all_cart', to: 'cart#index'
   post 'cart/add'
   post 'cart/remove'
-  resources :cart 
+
+  resources :cart
   resources :products do
     resources :reviews
   end
-  
-  devise_for :users, controllers: {registrations: 'users/registrations'}
+
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :products do 
-    member do 
+  resources :products do
+    member do
       get 'download_pdf'
-    end 
+    end
   end
   root to: 'products#index'
 end
